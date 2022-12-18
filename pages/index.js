b8dedb351850c7983f40af847bdece8c8ba4063a
Imac <imac@Imacs-iMac.local>
2022-12-18 17:21:45 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,21 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
-// export default function Component() {
-//   const { data: session } = useSession();
-//   console.log("session---> ", session);
-//   if (session) {
-//     return (
-//       <>
-//         Signed email as {session.user.email} <br />
-//         Signed name as {session.user.name} <br />
-//         <button onClick={() => signOut()}>Sign out</button>
-//       </>
-//     );
-//   }
-//   return (
-//     <>
-//       Not signed in <br />
-//       <button onClick={() => signIn()}>Sign in</button>
-//     </>
-//   );
-// }
-
-export default function Home() {
+export default function Component() {
+  const { data: session } = useSession();
+  console.log("session---> ", session);
+  if (session) {
+    return (
+      <>
+        Signed email as {session.user.email} <br />
+        Signed name as {session.user.name} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>
-      <h1>Next Js App</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit
-        voluptatibus commodi quia ullam itaque perferendis repellendus nobis
-        fugiat officiis. A dolore eveniet optio, sit excepturi numquam labore.
-        Et nesciunt vel laudantium inventore, suscipit, illo nisi ipsa cumque
-        rerum quae dolorem.
-      </p>
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
 }
